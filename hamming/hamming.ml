ab886b3283e0a14f9e591ce0946aed088120f27c
Hugbot <loganknecht@users.noreply.github.com>
2018-04-26 15:05:01 -0700

    Finally finished hamming.ml

--- hamming/hamming.ml
+++ hamming/hamming.ml
@@ -9,13 +9,24 @@ Assumes that the nucleotide_list_one and nucleotide_list_two are of equal length
 let hamming_distance (nucleotide_list_one : nucleotide list)
                      (nucleotide_list_two : nucleotide list)
                      : int option =
-    let length = List.length nucleotide_list_one in
-    let rec hamming_loop index hamming_sum = 
-        if index >= (length - 1)
-            then hamming_sum
-        else if (List.nth nucleotide_list_one index) != (List.nth nucleotide_list_two index)
-            then hamming_loop (index + 1) (hamming_sum + 1)
-        else
-            hamming_loop (index + 1) hamming_sum
-    in Some (hamming_loop 0 0)
+    let rec hamming_loop (list_one : nucleotide list)
+                         (list_two : nucleotide list)
+                         (hamming_sum : int)
+                         : int option = 
+        match list_one, list_two with
+        | [], [] -> Some hamming_sum
+        | [], _  -> Some hamming_sum
+        | _, []  -> Some hamming_sum
+        | list_one_head::list_one_tail, list_two_head::list_two_tail -> 
+            if (List.length list_one) <> (List.length list_two)
+                then None
+            else if list_one_head <> list_two_head
+                then (hamming_loop list_one_tail
+                                   list_two_tail
+                                   (hamming_sum + 1))
+            else
+                (hamming_loop list_one_tail
+                              list_two_tail
+                              hamming_sum)
+    in (hamming_loop nucleotide_list_one nucleotide_list_two 0)
 ;;
