0eabc3372a1e480ae937fbfb7f6b64e828476d14
Hugbot <loganknecht@users.noreply.github.com>
2018-04-26 16:09:07 -0700

    Updated leap.ml

--- leap/leap.ml
+++ leap/leap.ml
@@ -1,5 +1,6 @@
 let leap_year (year : int) : bool =
-    if (year mod 4) == 0 && ((year mod 100 != 0) || (year mod 400 == 0))
+    if (year mod 4) == 0 && 
+       ((year mod 100 <> 0) || (year mod 400 == 0))
         then true
     else
         false;;
