156c400394f4daffdebac850e613131b1392fdc8
Hugbot <loganknecht@gmail.com>
2018-04-29 16:12:12 -0700

    Format raindrops

--- raindrops/raindrops.ml
+++ raindrops/raindrops.ml
@@ -4,10 +4,10 @@
     If the number contains 7 as a factor, output 'Plong'.
     If the number does not contain 3, 5, or 7 as a factor, just pass the number's digits straight through.
 *)
-let mod_to_string (numerator)
-              (denominator: int)
-              (string_to_return : string)
-              : string =
+let mod_to_string (numerator : int)
+                  (denominator: int)
+                  (string_to_return : string)
+                  : string =
     if numerator mod denominator = 0
         then string_to_return
     else 
